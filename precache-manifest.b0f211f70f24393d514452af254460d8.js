3bf57f810711086f38b2a191a9d9d79c3bf3a988
Alvin Bierley <alvinbierley@Alvins-Air.home>
2020-03-29 03:48:36 -0700

    Updates

---
+++ precache-manifest.b0f211f70f24393d514452af254460d8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66bcf640572311745fc9e40936eefa39",
+    "url": "/scale/index.html"
+  },
+  {
+    "revision": "57609cf7a0b5659b0ca0",
+    "url": "/scale/static/css/main.3b38fd4d.chunk.css"
+  },
+  {
+    "revision": "80dae8631977890072cb",
+    "url": "/scale/static/js/2.9fd4a7e4.chunk.js"
+  },
+  {
+    "revision": "d83597d775d28f3887d670035b7da24f",
+    "url": "/scale/static/js/2.9fd4a7e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57609cf7a0b5659b0ca0",
+    "url": "/scale/static/js/main.be28fd14.chunk.js"
+  },
+  {
+    "revision": "d525225603b369c5fb4b",
+    "url": "/scale/static/js/runtime-main.25cf862e.js"
+  }
+]);
